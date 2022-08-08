116ef63671b6f100583406f93b36c975e7cfc3c3
Josh Goldberg <git@joshuakgoldberg.com>
2022-08-08 05:21:24 -0400

    Fix credits, and lib paths

--- fix-index-for-vercel.js
+++ fix-index-for-vercel.js
@@ -2,13 +2,40 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const libDir = path.join(__dirname, "lib");
+const indexPath = path.join(libDir, "index.html");
 
 // 1. Replace "../node_modules/"" paths with "./" in index.html
-const indexPath = path.join(libDir, "index.html");
-const indexContents = fs.readFileSync(indexPath).toString();
-fs.writeFileSync(indexPath, indexContents.replaceAll("../node_modules", "./lib"));
+fs.writeFileSync(
+    indexPath,
+    fs.readFileSync(indexPath).toString().replaceAll("../node_modules", "./")
+);
 
-// 2. Copy required node_modules/* packages into lib/
+// 2. Mess with lib/index.html's message
+fs.writeFileSync(indexPath, fixIndexContents(fs.readFileSync(indexPath).toString()));
+
+/** @param {string} contents */
+function fixIndexContents(contents) {
+    const lines = contents.split("\n");
+
+    lines.splice(
+        25,
+        9,
+        `<section id="social" class="section-text">
+        A fun little experiment by
+    <a class="link-github" href="http://joshuakgoldberg.com">Josh Goldberg</a>
+    (<a class="link-github" href="https://twitter.com/joshuakgoldberg">@JoshuaKGoldberg</a>) based on some
+    <a class="link-github" href="https://github.com/FullScreenShenanigans/FullScreenPokemon">very old code</a
+    >. 💖
+    </section>
+    <script>
+        console.log("Yes, performance is really bad. This is basically a dev build. I never got around to implementing production EightBittr builds...")
+    </script>`
+    );
+
+    return lines.join("\n");
+}
+
+// 3. Copy required node_modules/* packages into lib/
 const nodeModulesToCopy = [
     // Scaffolding
     "requirejs",
