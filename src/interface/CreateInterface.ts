e9b949b07615905239f44bdf662850f3e6c552fc
Josh Goldberg <git@joshuakgoldberg.com>
2022-08-22 09:30:52 -0400

    feat: friendly console log (#15)
    Overview
    What does this PR do? Why?
    Adds an nice little console log for people.
    PR Checklist
    
     Fixes FullScreenShenanigans/EightBittr#315, just for this repo
     I have run this code to verify it works
    [ ] This PR includes unit tests for the code change

--- src/interface/CreateInterface.ts
+++ src/interface/CreateInterface.ts
@@ -8,5 +8,33 @@ import { createUserWrapprSettings } from "./InterfaceSettings";
  * @param container   HTML element to create within.
  * @returns A Promise for creating the game interface.
  */
-export const createCypInterface = async (container: HTMLElement): Promise<void> =>
-    new UserWrappr(createUserWrapprSettings()).createDisplay(container);
+export const createCypInterface = async (container: HTMLElement): Promise<void> => {
+    const userWrapper = new UserWrappr(createUserWrapprSettings());
+
+    console.log(
+        `%c👋 Hello! Thanks for looking at ChooseYourFramework in your developer tools. ✨%c 
+The game is available as the global CYF variable. Its properties can be used to mess around with the game.
+For example:
+
+* %cCYF.frameTicker.setInterval(1);%c sets game speed to very fast)
+* %cCYF.frameTicker.setInterval(100);%c sets game speed to very slow
+* %cCYF.groupHolder.getActor("Lady").dialog = "All hail Lord Helix!";%c overrides that character's dialog
+
+See https://github.com/JoshuaKGoldberg/ChooseYourFramework for the game's source.
+See https://github.com/FullScreenShenanigans/EightBittr for its underlying game engine.
+
+%cEnjoy! 💖
+`,
+        "font-weight: bold",
+        "font-weight: normal",
+        "font-weight: bold",
+        "font-weight: normal",
+        "font-weight: bold",
+        "font-weight: normal",
+        "font-weight: bold",
+        "font-weight: normal",
+        "font-weight: bold"
+    );
+
+    return userWrapper.createDisplay(container);
+};
